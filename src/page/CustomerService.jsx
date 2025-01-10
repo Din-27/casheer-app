098af2b734a016c3bff35dac63389f94e392de32
herdin27 <it06.knitto@gmail.com>
2025-01-10 14:55:48 +0700

    add customer service

---
+++ src/page/CustomerService.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from "../components/Layout";
+
+export default function CustomerService() {
+    return (
+        <Layout>
+            <div className="flex flex-col bg-blue-gray-50 h-full w-full py-6 mx-4">
+                <h1 className="text-5xl font-bold">
+                    Setting
+                </h1>
+            </div>
+        </Layout>
+    )
+}
