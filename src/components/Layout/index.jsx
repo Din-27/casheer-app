6b284ca3be430e965689bb8d02471d8db4080928
Herdiyana Firmansyah <Herdiyana.firmansyah@it.component.astra.co.id>
2026-06-25 08:47:26 +0700

    fix: layout home page

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,22 +1,25 @@
-import { useSelector } from 'react-redux';
-import Sidebar from '../Sidebar/Sidebar'
-import SampleData from '../Modal/SampleData';
-import Receipt from '../Modal/Receipt';
+import { useSelector } from "react-redux";
+import Sidebar from "../Sidebar/Sidebar";
+import SampleData from "../Modal/SampleData";
+import Receipt from "../Modal/Receipt";
 
- 
 export default function Wrapper({ children }) {
-    const path = window.location.pathname
-    const { isShowModalReceipt, firstTime } = useSelector((state) => state.modal);
+  const path = window.location.pathname;
+  const { isShowModalReceipt, firstTime } = useSelector((state) => state.modal);
 
-    return (
-        <>
-            <div className={`${path.includes('setting') ? 'min-h-screen' : 'h-screen'} hide-print flex antialiased text-blue-gray-800`}>
-                <Sidebar />
-                {children}
-            </div>
-            {Boolean(firstTime) && <SampleData />}
-            {isShowModalReceipt && <Receipt />}
-            <div id="print-area" className="print-area"></div>
-        </>
-    )
+  return (
+    <>
+      <div
+        className={`${path.includes("setting") ? "min-h-screen" : "h-screen"} hide-print flex antialiased text-blue-gray-800`}
+      >
+        <Sidebar />
+        <div className="overflow-y-auto w-screen">
+          {children}
+          {Boolean(firstTime) && <SampleData />}
+          {isShowModalReceipt && <Receipt />}
+          <div id="print-area" className="print-area"></div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -4,41 +4,45 @@ import Table from "../components/Table/Table";
 import Pagination from "../components/Pagination/Pagination";
 
 export default function Home() {
+  return (
+    <Layout>
+      <div className="bg-blue-gray-50 h-screen w-full px-4 py-4 flex flex-col">
+        <h1 className="text-5xl font-bold mb-5">
+          Hallo, <span className="text-cyan-500">Herdiyana 👋</span>
+        </h1>
 
-
-    return (
-        <Layout>
-            <div className="bg-blue-gray-50 h-fit w-full py-6 mx-4 space-y-5">
-                {/* <Navbar /> */}
-                <h1 className="text-5xl font-bold">
-                    Hallo, <span className="text-cyan-500">Herdiyana 👋</span>
-                </h1>
-                <div className="space-y-6">
-                    <div className="grid grid-cols-4 gap-6">
-                        {[1, 1, 1, 1].map((x, y) => (
-                            <CardInfo
-                                key={y}
-                                percent={'+20%'}
-                                saldo={'1.500.000'}
-                                title={"Revenue"}
-                                icon={
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 24 24"
-                                        fill="currentColor"
-                                        className="icon icon-tabler icons-tabler-filled icon-tabler-coin"
-                                    >
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                        <path d="M17 3.34a10 10 0 1 1 -15 8.66l.005 -.324a10 10 0 0 1 14.995 -8.336zm-5 2.66a1 1 0 0 0 -1 1a3 3 0 1 0 0 6v2a1.024 1.024 0 0 1 -.866 -.398l-.068 -.101a1 1 0 0 0 -1.732 .998a3 3 0 0 0 2.505 1.5h.161a1 1 0 0 0 .883 .994l.117 .007a1 1 0 0 0 1 -1l.176 -.005a3 3 0 0 0 -.176 -5.995v-2c.358 -.012 .671 .14 .866 .398l.068 .101a1 1 0 0 0 1.732 -.998a3 3 0 0 0 -2.505 -1.501h-.161a1 1 0 0 0 -1 -1zm1 7a1 1 0 0 1 0 2v-2zm-2 -4v2a1 1 0 0 1 0 -2z" />
-                                    </svg>
-                                }
-                            />
-                        ))}
-                    </div>
-                    <Table data={[{ Order: 'burger' }]} />
-                    <Pagination />
-                </div>
+        <div className="flex-1 flex flex-col">
+          <div className="space-y-6 flex-1 overflow-auto">
+            <div className="grid grid-cols-4 gap-6">
+              {[1, 1, 1, 1].map((x, y) => (
+                <CardInfo
+                  key={y}
+                  percent="+20%"
+                  saldo="1.500.000"
+                  title="Revenue"
+                  icon={
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="icon icon-tabler icons-tabler-filled icon-tabler-coin"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                      <path d="M17 3.34a10 10 0 1 1 -15 8.66l.005 -.324a10 10 0 0 1 14.995 -8.336zm-5 2.66a1 1 0 0 0 -1 1a3 3 0 1 0 0 6v2a1.024 1.024 0 0 1 -.866 -.398l-.068 -.101a1 1 0 0 0 -1.732 .998a3 3 0 0 0 2.505 1.5h.161a1 1 0 0 0 .883 .994l.117 .007a1 1 0 0 0 1 -1l.176 -.005a3 3 0 0 0 -.176 -5.995v-2c.358 -.012 .671 .14 .866 .398l.068 .101a1 1 0 0 0 1.732 -.998a3 3 0 0 0 -2.505 -1.501h-.161a1 1 0 0 0 -1 -1zm1 7a1 1 0 0 1 0 2v-2zm-2 -4v2a1 1 0 0 1 0 -2z" />
+                    </svg>
+                  }
+                />
+              ))}
             </div>
-        </Layout>
-    );
+
+            <Table data={[{ Order: "burger" }]} />
+          </div>
+
+          <div className="mt-4 flex justify-start">
+            <Pagination />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
